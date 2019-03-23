83eff91a71591811f4a3425d99956cdf913afa2e
Liza Mihalevich <lizamih99@gmail.com>
2019-03-23 04:11:04 +0300

    added routers

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Authorization from './components/authorization/authorization';
+import Registration from './components/registration/registration';
 
 function App() {
     return (
         <div>
-            <Authorization />
+            <Switch>
+                <Route exact path="/" component={Authorization} />
+                <Route path="/login" component={Registration} />
+            </Switch>
         </div>
     );
 }
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
     window.document.getElementById('app'),
 );
